059482376265dfec73f11498f5d07053bc5f1af4
Userjoke <123212079+Jenyyk@users.noreply.github.com>
2024-09-19 17:53:32 +0200

    site rework first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+discord = document.getElementById("discord")
+discord.addEventListener("mouseenter", function() {
+  setTimeout(() => { discordReplaceText("userjoke") }, 200 )
+})
+discord.addEventListener("mouseleave", function() {
+  setTimeout(() => { discordReplaceText("Discord") }, 200 )
+})
+
+function discordReplaceText(string) {
+  text = discord.innerHTML.split(">")
+  text[1] = string
+  text = text.join(">")
+  console.log(text)
+  discord.innerHTML = text
+}
+
+landerDecor = document.getElementById("landerDecor")
+setInterval(function() {
+  newDiv = document.createElement("div")
+  newDiv.setAttribute("class","decorOffset")
+  landerDecor.appendChild(newDiv)
+  setTimeout(function() {document.querySelector(".decorOffset").remove()}, 2000)
+}, 500)
