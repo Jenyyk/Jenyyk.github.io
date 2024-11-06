c641bc5d85c87a0d149cf70267f040611d8eb5cd
Userjoke <123212079+Jenyyk@users.noreply.github.com>
2024-11-06 21:06:21 +0100

    cleaner design - way to go man

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-discord = document.getElementById("discord")
-discord.addEventListener("mouseenter", function() {
-  setTimeout(() => { discordReplaceText("userjoke") }, 200 )
-})
-discord.addEventListener("mouseleave", function() {
-  setTimeout(() => { discordReplaceText("Discord") }, 200 )
-})
-
-function discordReplaceText(string) {
-  text = discord.innerHTML.split(">")
-  text[1] = string
-  text = text.join(">")
-  discord.innerHTML = text
-}
-
 document.querySelectorAll(".Collider").forEach((collider) => collider.addEventListener("mousemove", function(e) {
   subject = collider.children[0]
   var subjectRect = subject.getBoundingClientRect()
