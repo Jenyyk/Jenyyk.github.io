3712252f8a93b5bcfb3b9ec6b80decc5418d297b
Userjoke <123212079+Jenyyk@users.noreply.github.com>
2024-11-09 23:37:25 +0100

    tech stack revamp

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-document.querySelectorAll(".Collider").forEach((collider) => collider.addEventListener("mousemove", function(e) {
-  subject = collider.children[0]
+document.querySelectorAll(".ColliderScName").forEach((collider) => collider.addEventListener("mousemove", function(e) {
+  rotateEffect(e, collider.children[0], 30)
+  
+}))
+document.querySelectorAll(".ColliderScName").forEach((collider) => collider.addEventListener("mouseleave", function() {
+  collider.children[0].style.transform = `rotate3d(0, 0, 0, 0deg)`
+}))
+
+function rotateEffect(e, subject, invIntensity) {
   var subjectRect = subject.getBoundingClientRect()
   centerX = (subjectRect.left + subjectRect.right) / 2
   centerY = (subjectRect.top + subjectRect.bottom) / 2
@@ -8,8 +15,5 @@ document.querySelectorAll(".Collider").forEach((collider) => collider.addEventLi
   yRatio = (e.clientY - subjectRect.top) / (subjectRect.bottom - subjectRect.top)
   yRatio = yRatio * 2 - 1
   rotRatio = (Math.abs(xRatio) + Math.abs(yRatio)) / 2
-  subject.style.transform = `rotate3d(${-yRatio}, ${xRatio}, 0, ${Math.sqrt(((centerX - e.clientX)**2) + ((centerY - e.clientY)**2))/30}deg)`
-}))
-document.querySelectorAll(".Collider").forEach((collider) => collider.addEventListener("mouseleave", function() {
-  collider.children[0].style.transform = `rotate3d(0, 0, 0, 0deg)`
-}))
+  subject.style.transform = `rotate3d(${-yRatio}, ${xRatio}, 0, ${Math.sqrt(((centerX - e.clientX)**2) + ((centerY - e.clientY)**2))/invIntensity}deg)`
+}
