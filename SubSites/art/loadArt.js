3aa16c8bc444ede7365ddba66e9e1baad42d9a4a
Jenyyk <jendotron2@gmail.com>
2026-01-26 21:59:41 +0100

    dev: ascii art page

---
+++ SubSites/art/loadArt.js
@@ -0,0 +1,57 @@
+loadArt("dio.txt", document.getElementById("dioTarget"))
+loadArt("diego.txt", document.getElementById("diegoTarget"))
+
+function loadArt(file, targetElement) {
+  fetch("./files/" + file)
+    .then(response => response.text())
+    .then(data => {
+      let maxLineLength = getArtLength(data);
+      console.log(maxLineLength);
+      targetElement.style.fontSize = `${100/maxLineLength/0.6}cqw`
+      targetElement.innerHTML = color(data);
+    });
+}
+
+function getArtLength(s) {
+  let maxLineLength = 0;
+  for (line of s.split("\n")) {
+    let rawLength = line.length;
+    for (char of line) {
+      if (char === 'ń') {
+        rawLength -= 7;
+      }
+    }
+    if (rawLength > maxLineLength) {
+      maxLineLength = rawLength;
+    }
+  }
+  return maxLineLength;
+}
+
+function htmlEscape(s) {
+  return s
+    .replaceAll(/&/g, "&amp;")
+    .replaceAll(/</g, "&lt;")
+    .replaceAll(/>/g, "&gt;")
+    .replaceAll(/"/g, "&quot;")
+    .replaceAll(/'/g, "&#039;");
+}
+
+function color(s) {
+  // Replace patterns like "<char>ńRRGGBB" with a colored span for the char.
+  // Operate on the raw input so that escaping is handled correctly for other characters.
+  const regex = /(.)ń([0-9A-Fa-f]{6})/g;
+  let out = '';
+  let lastIndex = 0;
+  let match;
+  while ((match = regex.exec(s)) !== null) {
+    const idx = match.index;
+    const ch = match[1];
+    const hex = match[2];
+    out += htmlEscape(s.substring(lastIndex, idx));
+    out += `<span style="color:#${hex};">${htmlEscape(ch)}</span>`;
+    lastIndex = idx + match[0].length;
+  }
+  out += htmlEscape(s.substring(lastIndex));
+  return out;
+}
